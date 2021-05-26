c88e83a36607e0ce00804771f5429c0676c73aaa
Nikita <83177719+NIKIT4NDR@users.noreply.github.com>
2021-05-26 23:22:54 +0500

    Add files via upload

---
+++ LovtsovPrakt3/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LovtsovPrakt3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 Задание\n");
+            int[] number = { 10, -3, -5, 2, 5 };
+            int min = number[0];
+            for (int i = 0; i < number.Length; i++)
+            {
+                if (number[i] < 0)
+                    number[i] *= -1;
+                if (min > number[i])
+                    min = number[i];
+            }
+            Console.WriteLine(min);
+            Console.ReadLine();
+            //2 Задание
+            Console.WriteLine("2 Задание\n");
+            int[] numbers = { 1, 5, 2, 1, 2, 3 };
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[i] == numbers[j])
+                    {
+                        Array.Clear(numbers, i, 1);
+                        Array.Clear(numbers, j, 1);
+                    }
+                }
+            }
+            foreach (var i in numbers)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+            //Задание 3
+            Console.WriteLine("Задание 3\n");
+            Random rnd = new Random();
+            int[] mass = new int[20];
+            for (int i = 0; i < mass.Length; i++)
+            {
+                mass[i] = rnd.Next(-100, 100);
+            }
+            for (int i = 0; i < mass.Length; i++)
+            {
+                if (mass[i] < 0)
+                {
+                    Array.Clear(mass, i, 1);
+                }
+            }
+            foreach (var i in mass)
+            {
+                Console.WriteLine(i);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PraktLovtsov1/PraktLovtsovDolgodusheva1/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace PraktLovtsovDolgodusheva1
+{
+    static class lib1
+    {
+        public static void stpow(string a,int b)
+        {
+            string str = "";
+            for (int i = 0; i < b; i++)
+            {
+                str += a;
+            }
+            Console.WriteLine(str + "\n");
+        }
+        public static void rev(string reversed)
+        {
+            string str = "";
+            for (int j = 1; j < reversed.Length +1 ; j++)
+            {
+                str += reversed[reversed.Length - j] ;
+            }
+            Console.WriteLine(str + "\n");
+        }
+        public static void remove(string removestr, string removestr1)
+        {
+            string str = removestr.Replace(removestr1,"");
+            Console.WriteLine(str + "\n");
+        }
+        public static void leng(string strlong)
+        {
+            int ij = strlong.Length;
+            Console.WriteLine(ij);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                string a;
+                int b;
+                Console.WriteLine("Введите значение А и В:");
+                a = Convert.ToString(Console.ReadLine());
+                b = Convert.ToInt32(Console.ReadLine());
+                lib1.stpow(a, b);
+            }
+            {
+                string reversed;
+                Console.WriteLine("Введите строку для реверсии:");
+                reversed = Console.ReadLine();
+                lib1.rev(reversed);
+            }
+            {
+                string removestr;
+                string removestr1;
+                Console.WriteLine("Введите строку и символ:");
+                removestr = Console.ReadLine();
+                removestr1 = Console.ReadLine();
+                lib1.remove(removestr, removestr1);
+            }
+            {
+                string strlong;
+                Console.WriteLine("Введите строку:");
+                strlong = Console.ReadLine();
+                lib1.leng(strlong);
+            }
+
+            Console.ReadLine();
+        }
+        
+    }
+}
