7e3dd53d39ada87fa40941a7f8a7231a8124d764
Nikita <83177719+NIKIT4NDR@users.noreply.github.com>
2021-05-27 13:32:57 +0500

    Add files via upload

---
+++ LovtsovPrakt10/LovtsovPrakt10/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LovtsovPrakt10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        a:
+            string vvod = Convert.ToString(Console.ReadLine());
+            int S = 0;
+            for (int i = 0; i < vvod.Length; i++)
+            {
+                if (vvod[i] != 'M' || vvod[i] != 'D' || vvod[i] != 'C' || vvod[i] != 'L' || vvod[i] != 'X' || vvod[i] != 'V' || vvod[i] != 'I')
+                {
+                    Console.WriteLine("Вы ввели недопустимый символ!");
+                    break;
+                }
+                if (vvod[i] == 'M')
+                {
+                    S += 1000;
+                }
+                if (vvod[i] == 'D')
+                {
+                    S += 500;
+                }
+                if (vvod[i] == 'C')
+                {
+                    S += 100;
+                }
+                if (vvod[i] == 'L')
+                {
+                    S += 50;
+                }
+                if (vvod[i] == 'X')
+                {
+                    S += 10;
+                }
+                if (vvod[i] == 'V')
+                {
+                    S += 5;
+                }
+                if (vvod[i] == 'I')
+                {
+                    S += 1;
+                }
+            }
+            if (S > 0)
+            {
+                Console.WriteLine(S);
+            }
+            Console.ReadLine();
+            goto a;
+        }
+    }
+}
---
+++ LovtsovPrakt4/LovtsovPrakt4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace LovtsovPrakt4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList() { "s", 5, 3, "E", "b", "S", 101, 1488, "ITDOG", 322, "qm2", "m3q", "MM", "e", 22 };
+            ArrayList LIST = new ArrayList();
+            string h = "";
+            foreach (var i in list)
+            {
+                Type t = i.GetType();
+                if (t.Equals(typeof(int)) == false)
+                {
+                    h += i + " ";
+                }
+            }
+            Console.WriteLine(h);
+            Console.ReadLine();
+            for (int i = 0; i < list.Count; i++)
+            {
+                if (Char.IsUpper(list[i].ToString().ToCharArray()[0]))
+                {
+                    LIST.Add(list[i]);
+                }
+            }
+            foreach (var i in LIST)
+            {
+                Console.WriteLine($"{i}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
